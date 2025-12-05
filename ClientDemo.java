a2f368090b555247fd04e26f8c926aecb2b94c26
Punit Tak <147526916+PunitTak2005@users.noreply.github.com>
2025-12-05 18:51:13 +0530

    Add ClientDemo class for socket communicationclient code of 3rd

---
+++ ClientDemo.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.net.*;
+
+public class ClientDemo {
+    public static void main(String[] args) throws Exception {
+        Socket s = new Socket("localhost", 5000);
+
+        PrintWriter pw = new PrintWriter(s.getOutputStream(), true);
+        pw.println("Hello Server, this is Client!");
+
+        s.close();
+    }
+}
