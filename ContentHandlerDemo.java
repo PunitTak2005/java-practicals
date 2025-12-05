4eb5de8a271072d8abf92629976bd0c23a3d638b
Punit Tak <147526916+PunitTak2005@users.noreply.github.com>
2025-12-05 20:37:03 +0530

    Add ContentHandlerDemo to read from a URL

---
+++ ContentHandlerDemo.java
@@ -0,0 +1,25 @@
+//2.3
+import java.net.*;
+import java.io.*;
+
+public class ContentHandlerDemo {
+    public static void main(String[] args) {
+        try {
+            URL url = new URL("https://example.com");
+
+            BufferedReader br = new BufferedReader(
+                    new InputStreamReader(url.openStream())
+            );
+
+            String line;
+            while ((line = br.readLine()) != null) {
+                System.out.println(line);
+            }
+
+            br.close();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+}
+
