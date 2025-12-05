6b03c746667687c2c12b4ef3fde4b67180b93081
Punit Tak <147526916+PunitTak2005@users.noreply.github.com>
2025-12-05 18:50:44 +0530

    server code of 3rd

---
+++ ServerDemo.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.net.*;
+
+public class ServerDemo {
+    public static void main(String[] args) throws Exception {
+        ServerSocket ss = new ServerSocket(5000);
+        System.out.println("Server started... Waiting for client...");
+
+        Socket s = ss.accept();
+        System.out.println("Client connected!");
+
+        BufferedReader br = new BufferedReader(
+                new InputStreamReader(s.getInputStream())
+        );
+
+        String msg = br.readLine();
+        System.out.println("Message received: " + msg);
+
+        ss.close();
+    }
+}
